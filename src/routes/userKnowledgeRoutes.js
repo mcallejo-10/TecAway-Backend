d3b978d8f8ba77799a9b17dfe5eea196a5c4805d
Miranda Callejón <mirandacallejonh@gmail.com>
2024-12-17 12:35:33 +0100

    arreglado error -kowledge por knowledge-, db funciona ok

--- src/routes/userKnowledgeRoutes.js
+++ src/routes/userKnowledgeRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getUserKowledge, getUserKnowledgeById , addUserKnowledge, deleteUserKnowledge} from '../controllers/userKnowledgeController.js';
+import { getUserKnowledge, getUserKnowledgeById , addUserKnowledge, deleteUserKnowledge} from '../controllers/userKnowledgeController.js';
 import { authenticateToken } from '../middlewares/authenticateToken.js';
 import { userKnowledgeValidator } from '../validations/userKnowledge.Validation.js';
 import { idValidator } from '../validations/generic.Validation.js'
@@ -7,10 +7,11 @@ import { idValidator } from '../validations/generic.Validation.js'
 const router = Router();
 
 // Rutas para obtener y modificar los datos de los usuarios
-router.get('/', authenticateToken(['user', 'admin']), getUserKowledge);
+router.get('/', authenticateToken(['user', 'admin']), getUserKnowledge);
 router.get('/:id', authenticateToken(['user', 'admin']), idValidator, getUserKnowledgeById);
 router.post('/', authenticateToken(['user', 'admin']), addUserKnowledge, userKnowledgeValidator);
 router.delete('/', authenticateToken(['user', 'admin']), deleteUserKnowledge);
 
 
 export default router;
+
