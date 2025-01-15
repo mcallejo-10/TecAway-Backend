dd4777c4ebd774b384897fe01830afc6be6b4da8
Miranda Callejón <mirandacallejonh@gmail.com>
2025-01-15 09:52:52 +0100

    Cloud config update

--- src/start_data.js
+++ src/start_data.js
@@ -58,7 +58,7 @@ const insertInitialUserData = async () => {
 
     {
       email: "mirandacallejonh@gmail.com",
-      password: "isma123",
+      password: "$2b$10$tXrqo7VdSPCLAsIUhrVsYejYeMt9FLo9J4OchgCKwuDvpeDK6Xf1q",
       name: "Miranda",
       title: "Técnica de luces y con experiencia en sonido",
       description: "técnica de luces formada en la EMAV. Ccon una amplia trayectoria como técnica en el mundo del teatro. Lorem ipsum On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish.",
