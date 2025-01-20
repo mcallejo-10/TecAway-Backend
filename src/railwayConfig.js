b0e24b94f9ee27f175c67fec0984672d543b58df
Miranda <mirandacallejonh@gmail.com>
2025-01-20 22:56:20 +0100

    env updated

---
+++ src/railwayConfig.js
@@ -0,0 +1,8 @@
+import e from "express";
+
+export const PORT = process.env.PORT || 3000;
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+export const DB_NAME = process.env.DB_NAME || 'tecaway'
+export const DB_PORT = process.env.DB_PORT || '3000';
