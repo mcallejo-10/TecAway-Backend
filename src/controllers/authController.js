29245dd8771cd0513f40a7acf9f0da087681cd1a
Miranda <mirandacallejonh@gmail.com>
2025-01-22 01:34:02 +0100

    serialize updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -120,7 +120,7 @@ export const login = async (req, res) => {
     const token = serialize('token', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 60 * 60 * 24 * 30,
       path: '/',
     });
