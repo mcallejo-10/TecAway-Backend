948b76ff3062f0d2432746cc778bb831fe9cf3a9
Miranda <mirandacallejonh@gmail.com>
2025-03-08 11:47:57 +0100

    addUserKnowledgeByAdmin route fixed

--- src/routes/userKnowledgeRoutes.js
+++ src/routes/userKnowledgeRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getUserKnowledgeById , addUserKnowledge, deleteUserKnowledge, getAllUserKnowledges} from '../controllers/userKnowledgeController.js';
+import { getUserKnowledgeById , addUserKnowledge, deleteUserKnowledge, getAllUserKnowledges, addUserKnowledgeByAdmin} from '../controllers/userKnowledgeController.js';
 import { authenticateToken } from '../middlewares/authenticateToken.js';
 import { userKnowledgeValidator } from '../validations/userKnowledge.Validation.js';
 
@@ -8,7 +8,7 @@ const router = Router();
 router.get('/', getAllUserKnowledges);
 router.get('/user', authenticateToken(['user', 'admin']), getUserKnowledgeById);
 router.post('/', authenticateToken(['user', 'admin']),  addUserKnowledge);
-router.post('/add-by-admin', authenticateToken(['user', 'admin']),  addUserKnowledge);
+router.post('/add-by-admin', authenticateToken(['user', 'admin']), addUserKnowledgeByAdmin);
 router.delete('/', authenticateToken(['user', 'admin']), deleteUserKnowledge);
 
 
